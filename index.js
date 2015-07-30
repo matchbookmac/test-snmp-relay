a54ad20ec27ad86c44e7d78d759d5b2f2dbbaec8
Ian @ epicodus <ian@iancmacdonald.com>
2015-07-30 10:57:30 -0700

    Relays trap reception as post request to external node server

--- index.js
+++ index.js
@@ -6,26 +6,13 @@ var
   express = require('express')
 ;
 var
-  app = express(),
   trapd = snmp.createTrapListener(),
   result = [],
-  messages = []
+  messages = [];
 ;
 
-app.use(express.static('public'));
-app.get('/get_today_count', function(req, res) {
-  //  console.log(result.length);
-  res.send(JSON.stringify(
-    { count: result.length.toString(),
-      message: messages[result.length - 1]
-    }));
-});
-
-var server = app.listen(3001, function() {
-  console.log('Listening on port %d', server.address().port);
-});
-
 trapd.on('trap', function (msg) {
+  // Find the message from the trap
   var trapValue = util.inspect(
     snmp.message
         .serializer(msg)['pdu']
@@ -35,12 +22,47 @@ trapd.on('trap', function (msg) {
     null
   );
 
+  // Push trap to ongoing result count, and current message to message array
   result.push(msg);
   messages.push(trapValue);
 
+  // Create postData for http POST
+  var postData = JSON.stringify({
+    count: result.length,
+    message: messages[result.length - 1]
+  });
+
+  // Log trap data
   var now = new Date();
-  console.log("Trap Received " + now);
-  console.log(trapValue);
-  console.log(result.length);
+  console.log("\n\n-------- \nTrap Received " + now);
+  console.log("Trap Value: " + trapValue);
+  console.log("Total Traps Received: " + result.length + "\n");
+
+  // Create POST request to external server
+  var options = {
+    hostname: "localhost",
+    port: 3002,
+    path: "/incoming-snmp",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  };
+
+  var req = http.request(options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    res.setEncoding('utf8');
+    res.pipe(process.stdout);
+  });
+
+  req.on("error", function (err) {
+    console.log("problem with request: " + err.message);
+  });
+
+  req.write(postData);
+  req.end();
+  // END POST
 });
+
+// bind listener to port receiving snmp traps
 trapd.bind({ family: 'udp4', port: 3000 });
